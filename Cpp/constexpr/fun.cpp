8d56a4f180e148bce78673424bb432b8e9a3f0d7
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-03 18:59:42 +0000

    Proper typing

--- Cpp/constexpr/fun.cpp
+++ Cpp/constexpr/fun.cpp
@@ -1,10 +1,10 @@
 // constexpr compile-time constant signature f(x)
-constexpr int factorial(int N) {
+constexpr unsigned int factorial(unsigned int N) {
 	return (N != 0) ? N*factorial(N-1) : 1;
 }
 
 int main() {
-	constexpr int magic = factorial(5); // compile-time calculated constant
-	constexpr int magic2 = factorial(10); // compile-time calculated constant
-	int nonmagic = factorial(2); // runtime assign
+	constexpr unsigned int magic = factorial(5); // compile-time calculated constant
+	constexpr unsigned int magic2 = factorial(10); // compile-time calculated constant
+	unsigned int nonmagic = factorial(2); // runtime assign
 }
--- Cpp/constexpr/struct.cpp
+++ Cpp/constexpr/struct.cpp
@@ -1,18 +1,18 @@
 // constexpr compile-time constant, signature f<x>::value
 // https://en.wikipedia.org/wiki/Template_(C%2B%2B)#Generic_programming_features_in_other_languages
-template<int N>
+template<unsigned int N>
 struct factorial {
-	static const int value = N*factorial<N-1>::value;
-	// alternatively static const int value = (N != 0) ? N*factorial<N-1>::value : 1;
+	enum { value = N*factorial<N - 1>::value };
+	//alternatively enum { value = (N != 0) ? N*factorial<N-1>::value : 1; }
 };
 
 template<>
 struct factorial<0> {
-	static const int value = 1;
+	enum { value = 1 };
 };
 
 int main() {
-	constexpr int magic = factorial<5>::value; // compile-time calculated constant
-	constexpr int magic2 = factorial<10>::value; // compile-time calculated constant
-	int nonmagic = factorial<2>::value;
+	constexpr unsigned int magic = factorial<5>::value; // compile-time calculated constant
+	constexpr unsigned int magic2 = factorial<10>::value; // compile-time calculated constant
+	unsigned int nonmagic = factorial<2>::value;
 }
