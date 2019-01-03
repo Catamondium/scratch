971e44e2b0ec311201103638c1c9a4c397b8b3e9
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-03 15:49:04 +0000

    Runtime assignment

--- Cpp/constexpr/fun.s
+++ Cpp/constexpr/fun.s
@@ -1,23 +1,24 @@
-	.file	"constexpr_struct.cpp"
+	.file	"fun.cpp"
 	.text
 	.globl	main
 	.type	main, @function
 main:
-.LFB0:
+.LFB1:
 	.cfi_startproc
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
 	.cfi_offset 6, -16
 	movq	%rsp, %rbp
 	.cfi_def_cfa_register 6
-	movl	$120, -8(%rbp)     # factorial<5>::value
-	movl	$3628800, -4(%rbp) # factorial<10>::value
+	movl	$120, -12(%rbp)    # factorial(5)
+	movl	$3628800, -8(%rbp) # factorial(10)
+	movl	$2, -4(%rbp)       # factorial(2) assigned
 	movl	$0, %eax
 	popq	%rbp
 	.cfi_def_cfa 7, 8
 	ret
 	.cfi_endproc
-.LFE0:
+.LFE1:
 	.size	main, .-main
 	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
 	.section	.note.GNU-stack,"",@progbits
--- Cpp/constexpr/struct.cpp
+++ Cpp/constexpr/struct.cpp
@@ -1,7 +1,9 @@
 // constexpr compile-time constant, signature f<x>::value
+// https://en.wikipedia.org/wiki/Template_(C%2B%2B)#Generic_programming_features_in_other_languages
 template<int N>
 struct factorial {
 	static const int value = N*factorial<N-1>::value;
+	// alternatively static const int value = (N != 0) ? N*factorial<N-1>::value : 1;
 };
 
 template<>
@@ -12,4 +14,5 @@ struct factorial<0> {
 int main() {
 	constexpr int magic = factorial<5>::value; // compile-time calculated constant
 	constexpr int magic2 = factorial<10>::value; // compile-time calculated constant
+	int nonmagic = factorial<2>::value;
 }
