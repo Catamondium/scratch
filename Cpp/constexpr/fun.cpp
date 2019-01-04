67bf19ceb4dac22d80e4ef9ef6eb4c20d019493c
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-04 13:53:16 +0000

    Included C++14 implementation

--- Cpp/constexpr/fun.cpp
+++ Cpp/constexpr/fun.cpp
@@ -1,6 +1,13 @@
 // constexpr compile-time constant signature f(x)
 constexpr unsigned int factorial(unsigned int N) {
-	return (N == 0) ? 1 : N*factorial(N-1);
+	// implementation since C++14 
+	unsigned int ret = 1;
+	for(unsigned int i = 1; i <= N; ++i) {
+		ret *= i;
+	}
+	return ret;
+	// C++11 implementation, return constrained
+	//return (N == 0) ? 1 : N*factorial(N-1);
 }
 
 int main() {
