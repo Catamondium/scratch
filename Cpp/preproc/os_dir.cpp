8e08429963b317ac71ad31e10fb9e962920b6a7d
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-05 22:48:29 +0000

    Multi-platform directives

---
+++ Cpp/preproc/os_dir.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+#ifdef __linux__ // linux
+	std::cout << "Linux" << std::endl;
+#endif
+
+#ifdef _WIN32    // windows 32 || 64 bit
+	std::cout << "Windows" << std::endl;
+#endif
+}
