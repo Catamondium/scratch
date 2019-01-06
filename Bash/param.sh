2342f2e7260f31ddf37d7b0d7111cfc52afd4435
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-06 23:20:34 +0000

    .

--- Bash/param.sh
+++ Bash/param.sh
@@ -2,5 +2,7 @@
 # parameter expansion
 # https://www.gnu.org/software/bash/manual/html_node/Shell-Parameter-Expansion.html
 
+# '#' match beginning pattern (match from beginning)
 echo ${1#*:} # print 1st param with shortest '*:' case omitted
+# '%' match trailing pattern  (match from end)
 echo ${1%:*} # print 1st param with shortest ':*' case omitted
