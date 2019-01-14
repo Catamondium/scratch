daa12a79f9b9fdf00484bfc4ce676c79c5a04437
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-01-14 23:15:46 +0000

    Made fmt more regular, is now constructible from const char* and convertible to std::string; += and + now perform concatenation.

--- Cpp/format.cpp
+++ Cpp/format.cpp
@@ -1,20 +1,39 @@
-#include <stdio.h>  // asprintf
+#include <stdio.h>     // asprintf
+#include <type_traits> // is_convertible
 #include <iostream>
 
 struct fmt {
 	std::string str;
-	fmt(std::string str): str(str) {};
-	template<class... Ts> std::string format(Ts...);
-	template<class... Ts> std::string operator()(Ts...);
+	fmt(const std::string &str) noexcept: str(str) {};
+	fmt(const char *c_str) noexcept: str(c_str) {};
+	template<class... Ts> std::string operator()(Ts...) noexcept;
+	template<class T> fmt& operator+=(const T&) noexcept;
+	template<class T> fmt operator+(const T&) const noexcept;
+	operator std::string() const { return str; }
 };
 
-fmt operator""_fmt(const char * str, std::size_t)
+template<class T>
+fmt& fmt::operator+=(const T &rhs) noexcept
+{
+	static_assert(std::is_convertible<T, fmt>::value, "Not convertible");
+	this->str += fmt(rhs).str;
+	return *this;
+}
+
+template<class T>
+fmt fmt::operator+(const T &rhs) const noexcept
+{
+	static_assert(std::is_convertible<T, fmt>::value, "Not convertible");
+	return fmt(*this) += fmt(rhs);
+}
+
+fmt operator""_fmt(const char * str, std::size_t) noexcept
 {
 	return {str};
 }
 
 template<class... Ts>
-inline std::string fmt::operator()(Ts... args)
+inline std::string fmt::operator()(Ts... args) noexcept
 {
 	char *buf;
 	int err;
@@ -30,5 +49,9 @@ inline std::string fmt::operator()(Ts... args)
 
 int main()
 {
-	std::cout << "_fmt:\t%05d"_fmt(5) << std::endl;
+	std::cout << "_fmt: %05d"_fmt(5) << std::endl;
+
+	fmt addtest = "Ass"; // const char* constructible
+	addtest += "Bass";   // Template allows all types, assert requires constructible/convertible
+	std::cout << "addtest: " << addtest() << std::endl;
 }
