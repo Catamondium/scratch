1d0451f52c187a881b50a7dc524f660c7ae43789
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-02-14 19:51:39 +0000

    now using mutex for clearer output

--- Cpp/multime.cpp
+++ Cpp/multime.cpp
@@ -1,10 +1,14 @@
-#include <thread>
 #include <chrono>
 #include <iostream>
 #include <vector>
 #include <utility>
 
+#include <thread>
+#include <mutex>
 
+std::mutex cout_rights; // reserve std::cout
+
+using timebase = std::chrono::seconds;
 using Runner = std::pair<int, std::string> ;
 std::vector<Runner> runners = 
 {
@@ -17,15 +21,21 @@ std::vector<std::thread> pool;
 
 void time(Runner run)
 {
+	auto before = std::chrono::system_clock::now();
 	std::this_thread::sleep_for(std::chrono::seconds(run.first));
-	std::cout << "TERM:\t" << run.second << std::endl;
+
+	// Aquire printing ability
+	std::lock_guard<std::mutex> guard(cout_rights);
+	auto elapsed = std::chrono::system_clock::now() - before;
+	std::cout << "TERM:\t" << run.second
+		<< "\t" << elapsed.count() / 1e9 << "s" << std::endl;
 }
 
-int main(int argc, char **argv)
+int main()
 {
+	std::thread Willy([](auto a){time(a);}, runners[2]);
 	std::thread Bob([](auto a){time(a);}, runners[0]);
 	std::thread Emma([](auto a){time(a);}, runners[1]);
-	std::thread Willy([](auto a){time(a);}, runners[2]);
 
 	Bob.join();
 	Emma.join();
