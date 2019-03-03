e1d0b04f3ec587155221dc67163d0b5812676e01
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-03-03 11:51:10 +0000

    .

--- Lisp/debug.lisp
+++ Lisp/debug.lisp
@@ -7,12 +7,13 @@
 (defmacro inspector-gadget (f)
  "Generates DEBUG-F for function F, wrapping it in INSPECT-CALL"
     (let ((observed-func (intern (concatenate   ; DEBUG-F
-                                'string
-                                (symbol-name :debug-)
-                                (write-to-string f)))))
+                                    'string
+                                    (symbol-name :debug-)
+                                    (write-to-string f)))))
         `(defun ,observed-func (&rest args)     ; (defun DEBUG-F (&rest args)
-            (apply #'inspect-call ',f args))))  ; 	(apply #'inspect-call 'F args))
+            (apply 'inspect-call ',f args))))  ; 	(apply #'inspect-call 'F args))
 
+(format t "~S~%" (macroexpand-1 '(inspector-gadget +)))
 (inspector-gadget +) ; DEBUG-+ returned to global scope
 (format t "Forwarded return: ~D~%" (debug-+ 2 2 2))
 (format t "~S~%" (documentation 'debug-+ 'function))
