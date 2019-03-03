67c88129c89841693bca4e88dbb3c7d51ac0c50e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-03-03 13:27:31 +0000

    Docstring generation

--- Lisp/debug.lisp
+++ Lisp/debug.lisp
@@ -1,7 +1,7 @@
 (defun inspect-call (f &rest args)
-    "Prints function call site and forwards it's return values"
+    "Prints function call site e.g (* 2 4) ==> 8 and forwards it's return values."
     (let ((ret (apply f args)))
-        (format t "(~S ~{~S~^ ~}) -> ~S~%" f args ret)
+        (format t "(~S ~{~S~^ ~}) ==> ~S~%" f args ret)
         ret))
 
 (defmacro inspector-gadget (f)
@@ -10,10 +10,14 @@
                                     'string
                                     (symbol-name :debug-)
                                     (write-to-string f)))))
-        `(defun ,observed-func (&rest args)     ; (defun DEBUG-F (&rest args)
-            (apply 'inspect-call ',f args))))  ; 	(apply #'inspect-call 'F args))
+        `(defun ,observed-func (&rest args)
+            ,(format nil "instrumented ~A, wrapped in ~A.~&~A: ~A~&~A: ~E"
+                f 'inspect-call f (documentation f 'function) 'inspect-call (documentation 'inspect-call 'function))
+            (apply 'inspect-call ',f args))))
 
 (format t "~S~%" (macroexpand-1 '(inspector-gadget +)))
 (inspector-gadget +) ; DEBUG-+ returned to global scope
 (format t "Forwarded return: ~D~%" (debug-+ 2 2 2))
-(format t "~S~%" (documentation 'debug-+ 'function))
+
+(defun add (&rest args) "Aliases + function." (apply '+ args))
+(format t "Doc: ~S" (documentation 'debug-add 'function))
