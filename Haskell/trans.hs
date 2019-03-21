3a7f8b2bdc4fcf941ca1d50feb55c051b3150e90
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-03-21 20:53:11 +0000

    .

--- Haskell/trans.hs
+++ Haskell/trans.hs
@@ -1,14 +1,17 @@
 trans :: Eq a => [a] -> [a] -> [a] -> [a]
+trans [] _ str = str
+trans _ [] str = str
 trans _ _ [] = []
 trans f t (c:str)
-    | c `elem` f    = sub   : trans f t str
-    | otherwise     = c     : trans f t str
+    | c `elem` cropf    = sub   : trans cropf t str
+    | otherwise         = c     : trans cropf t str
     where
         ft = zip f t
+        cropf = take (length t) f -- insure corresponding lengths
         Just sub = lookup c ft
 
-lookup :: Eq a => a -> [(a, b)] -> Maybe b
-lookup _ [] = Nothing
-lookup x ((a, b):lst)
+lookup' :: Eq a => a -> [(a, b)] -> Maybe b
+lookup' _ []    = Nothing
+lookup' x ((a, b):lst)
     | x == a    = Just b
     | otherwise = lookup x lst
