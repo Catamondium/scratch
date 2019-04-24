4e335b39bc70b0bd9011847fdf1699550edfbc63
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-04-24 20:15:38 +0100

    Generalization

--- Lisp/debug.lisp
+++ Lisp/debug.lisp
@@ -1,24 +1,31 @@
 (defun inspect-call (f &rest args)
-    "Prints function call site e.g (* 2 4) ==> 8 and forwards it's return values."
+    "Prints function call site e.g (* 2 4) ==> 8 and forwards it's return values"
     (let ((ret (apply f args)))
         (format t "(~S ~{~S~^ ~}) ==> ~S~%" f args ret)
         ret))
 
-(defmacro inspector-gadget (f)
- "Generates DEBUG-F for function F, wrapping it in INSPECT-CALL"
+(defun genwrap_gendoc (wrapper wrapee)
+    (concatenate
+        'string
+        (format nil "~A wrapped in ~A~&" wrapee wrapper)
+        (format nil "~A: ~A~&" wrapee (documentation wrapee 'function))
+        (format nil "~A: ~A" wrapper (documentation wrapper 'function))))
+
+(defmacro genwrap (wrapper wrapee)
+ "Generates DEBUG-WRAPEE for function WRAPEE, wrapping it in function WRAPEE"
     (let ((observed-func (intern (concatenate   ; DEBUG-F
                                     'string
-                                    (symbol-name :debug-)
-                                    (write-to-string f)))))
+                                    (write-to-string wrapper)
+                                    (symbol-name :-)
+                                    (write-to-string wrapee)))))
         `(defun ,observed-func (&rest args)
-            ,(format nil "instrumented ~A, wrapped in ~A.~&~A: ~A~&~A: ~A"
-                f 'inspect-call f (documentation f 'function) 'inspect-call (documentation 'inspect-call 'function))
-            (apply 'inspect-call ',f args))))
+            ,(genwrap_gendoc wrapper wrapee)
+            (apply ',wrapper ',wrapee args))))
 
-(format t "~S~%" (macroexpand-1 '(inspector-gadget +)))
-(inspector-gadget +) ; DEBUG-+ returned to global scope
-(format t "Forwarded return: (~{~S~^ ~}) ==> ~D~%~%" '(debug-+ 2 2 2) (debug-+ 2 2 2))
+(format t "~S~%" (macroexpand-1 '(genwrap inspect-call +)))
+(genwrap inspect-call +) ; inspect-call-+ returned to global scope
+(format t "Forwarded return: (~{~S~^ ~}) ==> ~D~%~%" '(inspect-call-+ 2 2 2) (inspect-call-+ 2 2 2))
 
-(defun add (&rest args) "Aliases + function." (apply '+ args))
-(inspector-gadget add)
-(format t "Doc: ~S" (documentation 'debug-add 'function))
+(defun add (&rest args) "Aliases + function" (apply '+ args))
+(genwrap inspect-call add)
+(format t "Doc inspect-call-add:~%~S" (documentation 'inspect-call-add 'function))
