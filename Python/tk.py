a0db10d1da7ebbe3b5455b3ee752d0ad7952f3e6
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-05-03 20:53:36 +0100

    .

--- Python/tk.py
+++ Python/tk.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python3
-from tkinter import *
+import tkinter as tk
 from functools import partial
 
 
-class App(Frame):
+class App(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
         self.pack()
 
-        self.top = Frame(master=self.master)
-        self.bottom = Frame(master=self.master)
+        self.top = tk.Frame(master=self.master)
+        self.bottom = tk.Frame(master=self.master)
         self.top.pack()
         self.bottom.pack()
 
-        self.num = Label(master=self.top)
+        self.num = tk.Label(master=self.top)
         self.num.pack()
 
-        self.var = IntVar()
+        self.var = tk.IntVar()
         self.var.set(0)
         self.num["textvariable"] = self.var
 
-        self.dec = Button(master=self.bottom, text="-", bg="red",
-                          command=partial(self.decrement, self))
-        self.inc = Button(master=self.bottom, text="+", bg="green",
-                          command=partial(self.increment, self))
+        self.dec = tk.Button(master=self.bottom, text="-", bg="red",
+                             command=partial(self.decrement, self))
+        self.inc = tk.Button(master=self.bottom, text="+", bg="green",
+                             command=partial(self.increment, self))
 
         self.dec.pack(side="left")
         self.inc.pack(side="left")
