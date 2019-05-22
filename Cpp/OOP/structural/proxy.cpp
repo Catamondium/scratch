1c7bfb5d3f4e460b9c7813ee7614b56610805e1e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-05-22 20:15:07 +0100

    .

---
+++ Cpp/OOP/structural/proxy.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <memory>
+using namespace std;
+
+struct Vehicle
+{
+    virtual void drive() = 0;
+};
+
+struct Car : Vehicle
+{
+    void drive() override
+    {
+        cout << "Driving car" << endl;
+    }
+};
+
+struct Bike : Vehicle
+{
+    void drive() override
+    {
+        cout << "Driving bike" << endl;
+    }
+};
+
+// Access control proxy
+template <class Impl>
+struct AgeProxy : Vehicle
+{
+    int age;
+    shared_ptr<Vehicle> vehicle = make_shared<Impl>();
+    AgeProxy(int age) : age(age){};
+
+    // Transparently delegates, depending on rights
+    void drive() final
+    {
+        if (age < 16)
+            cout << "Underage!" << endl;
+        else
+            vehicle->drive();
+    };
+};
+
+int main()
+{
+    Car normalcar;
+    AgeProxy<Car> agecar(7);
+    AgeProxy<Bike> agebike(18);
+
+    normalcar.drive();
+    agecar.drive();
+    agebike.drive();
+}
