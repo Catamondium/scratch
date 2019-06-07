dc49d63d37f146b059d251a2cb784374ce5db888
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-06-07 20:59:48 +0100

    .

--- Go/encoder/encoding/encoding.go
+++ Go/encoder/encoding/encoding.go
@@ -14,18 +14,24 @@ Encoder type
 import (
 	"fmt"
 	"reflect"
+	"strconv"
 )
 
-// Header information, reflected
-type Header struct {
-	name string
-	typ  reflect.Type
+// Heading information, reflected
+type Heading struct {
+	Name string
+	Type reflect.Type
 }
 
-// DeriveHeaders derive header information
+// True for named, settable fields
+func fieldReadable(v interface{}, f reflect.StructField) bool {
+	return !f.Anonymous
+}
+
+// DeriveHeader derive header information
 // from named public fields
-func DeriveHeaders(v interface{}) []Header {
-	out := make([]Header, 0)
+func DeriveHeader(v interface{}) []Heading {
+	out := make([]Heading, 0)
 	val := reflect.ValueOf(v)
 	if val.Kind() != reflect.Struct {
 		panic(fmt.Sprintf("Invalid kind %v", val.Kind()))
@@ -33,10 +39,51 @@ func DeriveHeaders(v interface{}) []Header {
 
 	for i := 0; i < val.NumField(); i++ {
 		f := val.Type().Field(i)
-		if !f.Anonymous {
-			out = append(out, Header{f.Name, f.Type})
+		if fieldReadable(v, f) {
+			out = append(out, Heading{f.Name, f.Type})
 		}
 	}
+	return out
+}
 
+func toString(v interface{}) string {
+	// complex number support?
+	// Tag conversion specifiers?
+	val := reflect.ValueOf(v)
+	switch n := val.Type().Name(); n {
+	case "string":
+		return v.(string)
+
+	case "int":
+		return strconv.FormatInt(int64(v.(int)), 10)
+	case "int8":
+		return strconv.FormatInt(int64(v.(int8)), 10)
+	case "int16":
+		return strconv.FormatInt(int64(v.(int16)), 10)
+	case "int32":
+		return strconv.FormatInt(int64(v.(int32)), 10)
+	case "int64":
+		return strconv.FormatInt(v.(int64), 10)
+
+	case "float32":
+		return strconv.FormatFloat(float64(v.(float32)), 'E', -1, 32)
+	case "float64":
+		return strconv.FormatFloat(v.(float64), 'E', -1, 64)
+
+	default:
+		panic(fmt.Sprintf("Type '%s' cannot strconv to string", n))
+	}
+}
+
+// MakeRecord create a **single** record from a struct and Headings
+func MakeRecord(v interface{}, header []Heading) []string {
+	val := reflect.ValueOf(v)
+	out := make([]string, 0)
+
+	for _, heading := range header {
+		field := val.FieldByName(heading.Name)
+		str := toString(field.Interface())
+		out = append(out, str)
+	}
 	return out
 }
--- Go/encoder/encoding/encoding_test.go
+++ Go/encoder/encoding/encoding_test.go
@@ -14,6 +14,8 @@ type Simple struct {
 	int  // anonymous field, ignored
 }
 
+const badPanic = "Failed to panic"
+
 const simpleSample = `
 Name|Freq|Val # REQUIRED
 Dan|5|55
@@ -27,15 +29,29 @@ func genReader(r io.Reader) *csv.Reader {
 	return ret
 }
 
+func sliceEqual(t *testing.T, exp, rec interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(exp, rec) {
+		t.Errorf("Expected: %#v\nRecieved: %#v", exp, rec)
+	}
+}
+
+func assertEqual(t *testing.T, exp, rec interface{}) {
+	t.Helper()
+	if !reflect.DeepEqual(exp, rec) {
+		t.Errorf("Expected: %#v Recieved: %#v", exp, rec)
+	}
+}
+
 func TestHeaderGen(t *testing.T) {
 	t.Run("Simple struct, 3 primitve, 1 anon field", func(t *testing.T) {
-		expected := []Header{
+		expected := []Heading{
 			{"Name", reflect.TypeOf("string()")},
 			{"Freq", reflect.TypeOf(int(0))},
 			{"Val", reflect.TypeOf(float64(0))},
 		}
 
-		recieved := DeriveHeaders(Simple{})
+		recieved := DeriveHeader(Simple{})
 
 		if !reflect.DeepEqual(expected, recieved) {
 			t.Errorf("Bad generation:\nExpected: %v\nRecieved: %v", expected, recieved)
@@ -48,7 +64,43 @@ func TestHeaderGen(t *testing.T) {
 			}
 		}()
 
-		DeriveHeaders(50) // should test all primatives, optimally
-		t.Errorf("Didn't panic")
+		DeriveHeader(50) // should test all primatives, optimally
+		t.Errorf(badPanic)
+	})
+}
+
+func TestRecordGen(t *testing.T) {
+	t.Run("Simple record generation", func(t *testing.T) {
+		source := Simple{"Adam", 50, 1.00, 0}
+		expected := []string{"Adam", "50", "1E+00"}
+
+		heading := DeriveHeader(source)
+		recieved := MakeRecord(source, heading)
+
+		sliceEqual(t, expected, recieved)
+	})
+}
+func TestToString(t *testing.T) {
+	t.Run("valid types", func(t *testing.T) {
+		assertEqual(t, "ABCD", toString("ABCD"))
+
+		assertEqual(t, "50", toString(50))
+		assertEqual(t, "50", toString(int8(50)))
+		assertEqual(t, "50", toString(int16(50)))
+		assertEqual(t, "50", toString(int32(50)))
+		assertEqual(t, "50", toString(int64(50)))
+
+		assertEqual(t, "1E+00", toString(1.00))
+		assertEqual(t, "1E+00", toString(float32(1.00)))
+	})
+
+	t.Run("panic on badKind", func(t *testing.T) {
+		defer func() {
+			if err := recover(); err != nil {
+			}
+		}()
+
+		toString(Simple{}) // should test all primatives, optimally
+		t.Errorf(badPanic)
 	})
 }
