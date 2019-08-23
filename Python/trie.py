cb795926f52a9f2a6f8ca3ad06ecee23f5760f96
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-08-23 01:32:12 +0100

    dict/set methods

--- Python/trie.py
+++ Python/trie.py
@@ -1,41 +1,72 @@
 #!/usr/bin/env python3
 class Trie:
-    def __init__(self, ch=None):
+    def __init__(self, ch=""):
         if ch is None:
             self.root = True
         else:
             self.root = False
+        self.isLeaf = False
         self.ch = ch
         self.children = {}
+        self.value = None
 
-    def insert(self, string: str):
+    def insert(self, string: str, value=None):
         if string == "":
+            self.isLeaf = True
+            self.value = value
             return
         try:
             self.children[string[0]].insert(string[1:])
         except KeyError:
             chld = Trie(string[0])
-            chld.insert(string[1:])
+            chld.insert(string[1:], value)
             self.children[string[0]] = chld
 
-    def find(self, string: str):
-        if string == "":
+    def __contains__(self, string: str):
+        if string == "" and self.isLeaf:
             return True
 
-        # if self.root:
         try:
-            return self.children[string[0]].find(string[1:])
+            return string[1:] in self.children[string[0]]
         except KeyError:
             return False
-        # else:
-        #  return self.children[string[0]].find(string[1:])
-        pass
+        except IndexError:
+            return False
+        return False
+
+    def keys(self, prec=""):
+        if self.isLeaf:
+            yield prec + self.ch
+
+        for chld in self.children.values():
+            yield from chld.keys(prec + self.ch)
+
+    def __iter__(self):
+        return self.keys()
+
+    def values(self, prec=""):
+        if self.isLeaf:
+            yield self.value
+
+        for chld in self.children.values():
+            yield from chld.values(prec + self.ch)
+
+    def pairs(self, prec=""):
+        if self.isLeaf:
+            yield (prec + self.ch, self.value)
+
+        for chld in self.children.values():
+            yield from chld.pairs(prec + self.ch)
 
     def print(self, indent=""):
         if self.root:
             print("R")
         else:
-            print(f"{indent}-{self.ch}")
+            if self.value:
+                leaf = f"{{{self.ch} : {self.value}}}"
+            else:
+                leaf = self.ch
+            print(f"{indent}-{leaf}")
         if len(self.children) != 0:
             print(f"{indent}\\")
         indent += " |"
@@ -45,9 +76,13 @@ class Trie:
 
 if __name__ == "__main__":
     t = Trie()
-    t.insert("ass")
+    t.insert("ass", 25)
     t.insert("abb")
     t.insert("bbb")
     t.print()
-    print(t.find("ass"))
-    print(t.find("cass"))
+    print("ass" in t)
+    print("cass" in t)
+    print("as" in t)
+    print(list(t.keys()))
+    print(list(t.values()))
+    print(list(t.pairs()))
