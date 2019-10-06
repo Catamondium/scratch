36bb549bd91c562c78823cbe0c256b52cdea0dfd
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-10-06 14:30:22 +0100

    tabs -> spaces

--- Lisp/loop.lisp
+++ Lisp/loop.lisp
@@ -1,17 +1,17 @@
 (defun make-multiples (n m)
-	(loop
-		for i from 0 upto n ; i = 0..n
-		collect             ; (*) i for each elem and collect
-			(loop for x in m collect (* x i))))
+  (loop
+    for i from 0 upto n ; i = 0..n
+    collect             ; (*) i for each elem and collect
+      (loop for x in m collect (* x i))))
 
 (setq i 5)
 (let
-	((fmt
-		(concatenate 'string
-			"~S:~%~{ (~{~"
-			(write-to-string (+ 1 (floor (log (* i i) 10))))
-			",'0D~^ ~})~&~}~&")))
+  ((fmt
+    (concatenate 'string
+      "~S:~%~{ (~{~"
+      (write-to-string (+ 1 (floor (log (* i i) 10))))
+      ",'0D~^ ~})~&~}~&")))
 
-		(format t fmt
-			`(make-multiples ,i ,(loop for x from 0 upto i collect x))
-			(make-multiples i  (loop for x from 0 upto i collect x))))
+    (format t fmt
+      `(make-multiples ,i ,(loop for x from 0 upto i collect x))
+      (make-multiples i  (loop for x from 0 upto i collect x))))
