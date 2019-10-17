fb5241853aec7e67bd0af92a20c182d2d23af61e
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-10-18 00:25:20 +0100

    fixes, refactoring

--- Python/tarcmd/tarcmd.py
+++ Python/tarcmd/tarcmd.py
@@ -2,6 +2,7 @@
 import tarfile as tf
 from cmd import Cmd
 from pathlib import Path
+from typing import List, Optional as Maybe
 from trie import Trie
 from copy import deepcopy
 import sys
@@ -38,7 +39,7 @@ editing: edit, mv, rm, add
 
 class Tarcmd(Cmd):
     intro = "Tar explorer shell"
-    postfix = "T> "
+    postfix = "(Tar) > "
     prompt = postfix
     tree = Trie()
 
@@ -48,11 +49,10 @@ class Tarcmd(Cmd):
     def refresh_prompt(self):
         self.prompt = f"{self.environ['pwd']} {self.postfix}"
 
-    def resolve(self, path='.'):
-        if not path:
-            return None
-        #elif path == '.':
-        #    return deepcopy(self.pwd)
+    def resolve(self, path: str = '.') -> Maybe[List[str]]:
+        """Resolve TAR paths"""
+        if not path or path == '.':
+            return deepcopy(self.pwd)
 
         p = Path(path)
         parts = [*p.parts]
@@ -60,11 +60,10 @@ class Tarcmd(Cmd):
 
         try:
             origin = parts[0]
-            if origin == '.':
+            if origin in ('~', '/'):
+                parts = parts[1:]
+            else:
                 resolved = deepcopy(self.pwd)
-                parts = parts[1:]
-            elif origin in ('~', '/'):
-                parts = parts[1:]
         except IndexError:
             pass
 
@@ -72,16 +71,15 @@ class Tarcmd(Cmd):
             if part in ('.', '~'):
                 # Ignore null move
                 continue
-            elif part == '..':
+            elif part == '..' and resolved:
                 # Remove previous, obeying root
-                if resolved:
-                    resolved.pop()
+                resolved.pop()
             else:
                 resolved.append(part)
 
-        return resolved or None
+        return resolved
 
-    def do_mount(self, target):
+    def do_mount(self, target: Path):
         """Mount a new tar archive"""
         self.environ['file'] = target
         self.environ['pwd'] = '/'
@@ -90,36 +88,47 @@ class Tarcmd(Cmd):
             for info in f:
                 self.tree[info.name.split('/')] = info
 
-    def do_ls(self, path, verbose=False):
+    def do_ls(self, path: str, verbose=False):
         """List members"""
         solved = self.resolve(path)
         p = Path('/'.join(solved or []))
         n = None
-        #try:
-        print(f"Resolved: {solved}")
-        n = self.tree.getNode(solved)
+        try:
+            print(f"Resolved: {solved}")
+            n = self.tree.getNode(solved)
 
-        results = [n, *n.children.values()]
-        results = [(i.value.name, i.value) for i in results if i.isLeaf]
+            results = [n, *n.children.values()]
+            results = [(i.value.name, i.value) for i in results if i.isLeaf]
 
-        for r, _ in sorted(results, key=lambda t: t[0]):
-            relative = Path(r).relative_to(p)
-            print(str(relative))
-        #except:
-        #    print(f"No such path: {n or str(path)}")
+            for r, _ in sorted(results, key=lambda t: t[0]):
+                relative = Path(r).relative_to(p)
+                print(str(relative))
+        except:
+            print(f"No such path: {n or str(path)}")
 
     def do_env(self, *args):
         """List environment variables"""
         for k, v in self.environ.items():
             print(f"{k} = {v}")
 
-    def do_cd(self, path):
-        # TODO
+    def do_cd(self, path: str):
+        """Change working directory"""
+        npwd = self.resolve(path)
+        self.pwd = npwd
+        self.environ['pwd'] = '/' + '/'.join(npwd)
         self.refresh_prompt()
 
     def do_debug(self, *args):
-        """Display underlying trie"""
+        print(f"pwd: {self.pwd}")
         self.tree.print()
+    
+    def do_exit(self, *args):
+        """Exit tarcmd"""
+        sys.exit()
+    
+    def do_quit(self, *args):
+        """Exit tarcmd"""
+        sys.exit()
 
 
 if __name__ == "__main__":
