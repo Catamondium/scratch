24134e0218cabf5514860a27fbb242cedccb1ba2
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-10-30 00:24:59 +0000

    weak table example

---
+++ Lua/weak.lua
@@ -0,0 +1,21 @@
+-- Weak tables, defaults example
+
+defaults = {}
+-- Weakly keyed
+setmetatable(defaults, {__mode = "k"})
+function setDefault(t, d)
+    defaults[t] = d
+    setmetatable(t, {__index = function (t, _) return defaults[t] end})
+end
+
+ttab = {}
+ttab['first'] = {a = 5, b = 10} -- bind table A
+setDefault(ttab['first'], 0) -- enroll
+for k,v in pairs(ttab['first']) do
+    print(k, v)
+end
+print(ttab['first'].c)
+
+ttab['first'] = {} -- A sieces use
+collectgarbage()
+for k,v in pairs(defaults) do print(k) end -- empty
