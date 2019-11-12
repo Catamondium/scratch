2a1281095f130db199096b4d68d949f4b8189e9d
Catamondium <30911915+Catamondium@users.noreply.github.com>
2019-11-12 21:54:11 +0000

    docs & linting

--- Nim/macro.nim
+++ Nim/macro.nim
@@ -1,27 +1,53 @@
 import macros
 import sequtils
 
-proc appendCall(para, callee: NimNode): NimNode =
+# Some arrow syntax from Lisp-syntaxes
+# Will NOT implement -<> magic wand
+# w/o a recursive subsitution function
+
+proc addCall(pre: static[bool] = true, para, callee: NimNode): NimNode =
+    ## insert syntactic para into call
     case callee.kind:
         of nnkIdent:
             result = newCall(callee, para)
         of nnkCall:
             result = newCall(callee[0])
+            when pre:
+                result.add(para)
+
             for arg in callee[1..^1]:
                 result.add(arg)
-            result.add(para)
+
+            when not pre:
+                result.add(para)
         else:
             raise newException(ValueError, "Invalid call")
 
-macro pThreaded(body: untyped): untyped =
+macro `->`(body: untyped): untyped =
+    ## Pre-inserting threading operator
     body.expectMinLen(1)
-    foldl(body, appendCall(a, b))
+    foldl(body, addCall(pre = true, a, b))
+
+macro `->>`(body: untyped): untyped =
+    ## Post-inserting threading operator
+    body.expectMinLen(1)
+    foldl(body, addCall(pre = false, a, b))
 
 proc passecho[T](a: T): T =
     echo a
     a
-let v = pThreaded:
-    5
-    `+`(3)
-    passecho
-echo v
+
+block:
+    echo "postarrow"
+    let v = `->>`:
+        5
+        `-`(3)
+        passecho
+    echo v
+block:
+    echo "prearrow"
+    let v = `->`:
+        5
+        `-`(3)
+        passecho
+    echo v
