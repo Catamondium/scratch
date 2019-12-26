c0b081da1fbddccd6e88e725912bfe859f59ff28
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2019-12-26 02:30:10 +0000

    Signal based ipc example

---
+++ C/sigipc.c
@@ -0,0 +1,26 @@
+#include <signal.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+// posix signalling as ipc example
+// https://stackoverflow.com/questions/21542077/c-sigalrm-alarm-to-display-message-every-second
+
+volatile sig_atomic_t print_flag = false;
+
+void handle_alarm( int sig ) {
+    print_flag = true;
+}
+
+int main() {
+    signal( SIGALRM, handle_alarm ); // Install handler first,
+    alarm( 1 ); // before scheduling it to be called.
+
+    for (;;) {
+        if ( print_flag ) {
+            printf( "Hello\n" );
+            print_flag = false;
+            alarm( 1 );
+        }
+    }
+}
