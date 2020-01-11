7d990b75671d52fdede5fc39b992400a471c14cf
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2020-01-11 23:09:04 +0000

    Factored 'Task' retrieval to global

--- Python/flask/basic/tasks_api.py
+++ Python/flask/basic/tasks_api.py
@@ -6,11 +6,13 @@ tasks = Blueprint('tasks', __name__)
 @tasks.record  # on application reg
 def record(state):
     assert 'sqlalchemy' in state.app.extensions, "SQLAlchemy required"
+    global Task
+    Task = state.app.config['tasks.db']
 
 
 @tasks.route('/')
 def listall():
-    Task = current_app.config['tasks.db']
+#    Task = current_app.config['tasks.db']
     tasks = Task.query.order_by(Task.priority.desc()).all()
     return jsonify([x.todict() for x in tasks])
 
@@ -22,7 +24,7 @@ def single(task=None, priority=0):
         return make_response('Name required', 400)
 
     task = task.strip()
-    Task = current_app.config['tasks.db']
+#    Task = current_app.config['tasks.db']
     session = current_app.extensions['sqlalchemy'].db.session
     if request.method == 'GET':  # fetch individual Task
         target = Task.query.filter_by(name=task).first_or_404()
