0b1bb3b82325681204c18525c663f19e91e2da47
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2020-01-22 21:14:37 +0000

    Usage of anonymous union and struct

---
+++ C/sizes.c
@@ -0,0 +1,29 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Basic for for sizeof test
+typedef struct {
+    int x, y;
+} Vec;
+
+// Form providing alternative 'views'
+typedef struct {
+    union {
+	// see Vec
+	struct {
+	    int x, y;
+	};
+	// as seq of ints
+	int raw[2];
+    };
+} VecUn;
+
+int main()
+{
+    assert(sizeof(Vec) == 2 * sizeof(int));
+    printf("Views example\n");
+
+    VecUn v = {.x = 1,.y = 2 };
+    printf(".x = %d, .y = %d\n", v.x, v.y);
+    printf("[0] = %d, [1] = %d\n", v.raw[0], v.raw[1]);
+}
