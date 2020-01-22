4ce725a920f77430240f2c6f74ee9df28fb54a15
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2020-01-22 01:36:33 +0000

    Added lisp style pprint

--- C/bison/main.c
+++ C/bison/main.c
@@ -8,8 +8,10 @@
 #include "Lexer.h"
 
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
-int yyparse(SExpression **expression, yyscan_t scanner);
+int yyparse(SExpression ** expression, yyscan_t scanner);
 
 SExpression *getAST(const char *expr)
 {
@@ -18,15 +20,15 @@ SExpression *getAST(const char *expr)
     YY_BUFFER_STATE state;
 
     if (yylex_init(&scanner)) {
-        /* could not initialize */
-        return NULL;
+	/* could not initialize */
+	return NULL;
     }
 
     state = yy_scan_string(expr, scanner);
 
     if (yyparse(&expression, scanner)) {
-        /* error parsing */
-        return NULL;
+	/* error parsing */
+	return NULL;
     }
 
     yy_delete_buffer(state, scanner);
@@ -36,25 +38,72 @@ SExpression *getAST(const char *expr)
     return expression;
 }
 
-int evaluate(SExpression *e)
+int evaluate(SExpression * e)
 {
     switch (e->type) {
-        case eVALUE:
-            return e->value;
-        case eMULTIPLY:
-            return evaluate(e->left) * evaluate(e->right);
-        case eADD:
-            return evaluate(e->left) + evaluate(e->right);
-        default:
-            /* should not be here */
-            return 0;
+    case eVALUE:
+	return e->value;
+    case eMULTIPLY:
+	return evaluate(e->left) * evaluate(e->right);
+    case eADD:
+	return evaluate(e->left) + evaluate(e->right);
+    default:
+	/* should not be here */
+	return 0;
     }
+}
+
+#define STR_S 3
+
+char *extend(char *indent)
+{
+    size_t size = strlen(indent) + 2 + STR_S;
+    char *new = malloc(sizeof(char) * size);
+    assert(new != NULL);
+    strcpy(new, indent);
+    strcat(indent, "  ");
+    return new;
+}
+
+void recur(SExpression *e, char *op);
+
+void _plisp(SExpression * e, int outer)
+{
+    switch (e->type) {
+    case eVALUE:
+	printf("%d", e->value);
+	break;
+    case eADD:
+	recur(e, "+");
+	break;
+    case eMULTIPLY:
+	recur(e, "*");
+	break;
+    }
+
+    if (outer)
+	putchar('\n');
+}
+
+void recur(SExpression *e, char *op)
+{
+    printf("(%s ", op);
+    _plisp(e->left, 0);
+    printf(" ");
+    _plisp(e->right, 0);
+    printf(")");
+}
+
+void plisp(SExpression *e)
+{
+    _plisp(e, 1);
 }
 
 int main(void)
 {
     char test[] = " 4 + 2*10 + 3*( 5 + 1 )";
     SExpression *e = getAST(test);
+    plisp(e);
     int result = evaluate(e);
     printf("Result of '%s' is %d\n", test, result);
     deleteExpression(e);
