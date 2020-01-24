54330ec86700feb32a8dc1fdd1e6df8cf21bb33c
Liam Birtles <30911915+Catamondium@users.noreply.github.com>
2020-01-24 00:58:43 +0000

    Renaming and documentation(?)

--- Haskell/logmon.hs
+++ Haskell/logmon.hs
@@ -1,39 +1,39 @@
-newtype Logged w a = Logged { runLogged :: (a, w)} deriving Show
+newtype Logger w a = Logger { runLogger :: (a, w)} deriving Show
 
-instance Functor (Logged w) where
-    fmap f (Logged (x, log)) = Logged (f x, log)
+instance Functor (Logger w) where
+    fmap f (Logger (x, log)) = Logger (f x, log)
 
-instance Monoid w => Applicative (Logged w) where
-    pure x = Logged (x, mempty) -- provide new log
+instance Monoid w => Applicative (Logger w) where
+    pure x = Logger (x, mempty) -- provide new log
     -- breaking rules?
     mf <*> ma =
-       Logged ((f a), log1 `mappend` log2) -- maintain both logs?
-           where (f, log1) = runLogged mf
-                 (a, log2) = runLogged ma
+       Logger ((f a), log1 `mappend` log2) -- maintain both logs?
+           where (f, log1) = runLogger mf
+                 (a, log2) = runLogger ma
 
-instance Monoid w => Monad (Logged w) where
+instance Monoid w => Monad (Logger w) where
     ma >>= fmb =
-        Logged (b, log1 `mappend` log2) -- also maintain both logs?
-            where (x, log1) = runLogged ma
-                  (b, log2) = runLogged (fmb x)
+        Logger (b, log1 `mappend` log2) -- also maintain both logs?
+            where (x, log1) = runLogger ma
+                  (b, log2) = runLogger (fmb x)
 
-tell :: Monoid w => w -> Logged w ()
-tell str = Logged ((), str)
+tell :: Monoid w => w -> Logger w ()
+tell str = Logger ((), str)
 
-reversed' :: Show a => [a] -> Logged [[a]] [a]
-reversed' [] = return []
-reversed' (x:xs) =
+reverse' :: Show a => [a] -> Logger [[a]] [a]
+reverse' [] = return []
+reverse' (x:xs) =
     do
-        inner <- reversed' xs
-        -- >>= passes log along
+        inner <- reverse' xs
+        -- (>>=) passes log
         let rslt = inner ++ [x]
-         in do
-             tell [rslt]
-             return rslt
+        tell [rslt]
+        -- (>>) passes log?
+        return rslt
 
 main = do
     putStrLn ("in: " ++ show xs)
     print log
         where
             xs = [1, 2, 3, 4]
-            log = reversed' xs
+            log = reverse' xs
